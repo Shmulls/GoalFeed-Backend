0b18d2df2fd949aef4d5368abed8bfa1b5133a69
Shmuel Malikov <sshmoel1@gmail.com>
2023-05-22 21:19:57 +0300

    route added.

--- routes/posts.js
+++ routes/posts.js
@@ -6,18 +6,22 @@ import {
   deletepost,
   editPost,
   getPostsilike,
+  getSavePosts,
+  savePost,
 } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /* READ */
+router.get("/:userId/posts-ilike", verifyToken, getPostsilike);
+router.get("/:userId/posts-saved", verifyToken, getSavePosts);
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
-router.get("/:userId/posts-ilike", verifyToken, getPostsilike);
 
 /* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/save", verifyToken, savePost);
 router.patch("/:id/edit", verifyToken, editPost);
 
 /* DELETE */
